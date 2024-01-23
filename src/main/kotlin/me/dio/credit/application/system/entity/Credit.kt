8b1a5857d6be30522717c0f45e07ec9b7174bfae
otonielnn <otoniel_2002@live.com>
2024-01-23 03:35:19 -0300

    Adicionando annotations as entities e criando repositories

--- src/main/kotlin/me/dio/credit/application/system/entity/Credit.kt
+++ src/main/kotlin/me/dio/credit/application/system/entity/Credit.kt
@@ -1,14 +1,19 @@
 package me.dio.credit.application.system.entity
 
+import jakarta.persistence.*
+import me.dio.credit.application.system.enumeration.Status
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.util.*
 
+@Entity
+@Table(name = "Credit")
 data class Credit(
-    val creditCode: UUID = UUID.randomUUID(),
-    val creditValue : BigDecimal = BigDecimal.ZERO,
-    val dayFirstInstallment: LocalDate,
-    val numberOfInstallments: Int = 0,
-    val status: Status,
-
-)
+    @Column(nullable = false, unique = true) val creditCode: UUID = UUID.randomUUID(),
+    @Column(nullable = false) val creditValue : BigDecimal = BigDecimal.ZERO,
+    @Column(nullable = false) val dayFirstInstallment: LocalDate,
+    @Column(nullable = false) val numberOfInstallments: Int = 0,
+    @Enumerated val status: Status,
+    @ManyToOne val customer: Customer? = null,
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null
+    )
