a93148b009206bacaa3b925000930884e29802fd
otonielnn <otoniel_2002@live.com>
2024-01-26 04:55:15 -0300

    Adicionando DTO's e Controllers

--- src/main/kotlin/me/dio/credit/application/system/entity/Credit.kt
+++ src/main/kotlin/me/dio/credit/application/system/entity/Credit.kt
@@ -9,11 +9,11 @@ import java.util.*
 @Entity
 @Table(name = "Credit")
 data class Credit(
-    @Column(nullable = false, unique = true) val creditCode: UUID = UUID.randomUUID(),
+    @Column(nullable = false, unique = true) var creditCode: UUID = UUID.randomUUID(),
     @Column(nullable = false) val creditValue : BigDecimal = BigDecimal.ZERO,
     @Column(nullable = false) val dayFirstInstallment: LocalDate,
     @Column(nullable = false) val numberOfInstallments: Int = 0,
-    @Enumerated val status: Status,
+    @Enumerated var status: Status = Status.IN_PROGRESS,
     @ManyToOne var customer: Customer? = null,
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long? = null
     )
